be1ed114d42ad89ef214b4ee356eb626162e86fa
Alireza Fatemi <alirezafa@rocketmail.com>
2020-01-20 12:34:03 +0330

    :sparkles: About.

--- src/components/sections/AboutSection.js
+++ src/components/sections/AboutSection.js
@@ -5,21 +5,26 @@ import quoteOff from "../../images/quote-off.jpg"
 import { makeStyles, useTheme } from "@material-ui/core"
 
 const useStyles = makeStyles(theme => ({
+  about: {
+    textAlign: "justify",
+  },
   image: {
     margin: "auto",
     display: "block",
     borderRadius: 8,
+    marginBottom: theme.spacing(2),
   },
 }))
 
 // https://codepen.io/_Sabine/pen/EryxNr
+// https://codepen.io/screeny05/pen/rxObWx?q=quote%20author&limit=all&order=popularity&depth=everything&show_forks=falsehttps://codepen.io/screeny05/pen/rxObWx
 export function AboutSection() {
   const theme = useTheme()
   const classes = useStyles()
   const isDark = theme.palette.type === "dark"
   const quote = isDark ? quoteOff : quoteOn
   return (
-    <div>
+    <div className={classes.about}>
       <img src={quote} width={600} className={classes.image} />
       {/*<div className="quote--container">*/}
       {/*  <p className="quote">*/}
@@ -27,7 +32,20 @@ export function AboutSection() {
       {/*  </p>*/}
       {/*  <p className="quote--author">&ndash; Francis Bacon</p>*/}
       {/*</div>*/}
-
+      Welcome! My name is Seyed Alireza Fatemi Jahromi. I'm currently a bachelor of Computer Engineering at Sharif
+      University of Technology. My main research focus is on Deep Learning & Machine Learning. My experience ranges from
+      designing and implementing mobile and web applications to designing and implementing various complex deep learning
+      algorithms. I love to enter new fields of artificial intelligence and play with new datasets and applications of
+      deep learning. One of my hobbies is reading papers about fascinating applications of deep learning algorithms in
+      various fields.
+      <br />
+      I have worked with many programming languages and frameworks, and learning to use a new programming language or
+      tool doesn't take too much time and effort for me. I love to learn new things, and I'm a fast learner. You can
+      find the complete list of the tools I've worked with in my <a href={"/Seyed Alireza Fatemi Jahromi.pdf"}>CV</a>.
+      <br />
+      I love playing games, watching series, running, and swimming. My favorite games are "The Elder Scrolls V: Skyrim"
+      & "Control" and my favorite series are "The OA", "Looking for Alaska", "Sharp Objects", and "The Witcher". I love
+      to travel to new places and meet new people.
     </div>
   )
 }
