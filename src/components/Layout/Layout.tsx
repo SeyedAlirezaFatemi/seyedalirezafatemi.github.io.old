9ae58dcf85ab34e8f3c56211958a035a5cd470cf
Alireza Fatemi <alirezafa@rocketmail.com>
2020-10-20 00:39:06 +0330

    :ambulance: Critical hotfix.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -76,21 +76,13 @@ const useStyles = makeStyles(theme => ({
 }))
 
 function getInitialColorMode(): string {
-  const persistedColorPreference =
-    window.localStorage.getItem("theme") //color-mode
-  const hasPersistedPreference =
-    typeof persistedColorPreference === "string"
-  // If the user has explicitly chosen light or dark,
-  // let's use it. Otherwise, this value will be null.
-  if (typeof persistedColorPreference === "string") {
-    return persistedColorPreference
-  }
-  // If they haven't been explicit, let's check the media
-  // query
-  const mql = window.matchMedia("(prefers-color-scheme: dark)")
-  const hasMediaQueryPreference = typeof mql.matches === "boolean"
-  if (hasMediaQueryPreference) {
-    return mql.matches ? "dark" : "light"
+  // Let's check the media query
+  if (typeof window !== 'undefined') {
+    const mql = window.matchMedia("(prefers-color-scheme: dark)")
+    const hasMediaQueryPreference = typeof mql.matches === "boolean"
+    if (hasMediaQueryPreference) {
+      return mql.matches ? "dark" : "light"
+    }
   }
   // If they are using a browser/OS that doesn't support
   // color themes, let's default to 'light'.
